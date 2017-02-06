e1bcedb469a28c953682b39d622564fdf217e66e
rocky <rocky.luo@ximalaya.com>
2017-02-06 17:12:45 +0800

    修改txt

--- src/main/java/com/rocky/web/HelloController.java
+++ src/main/java/com/rocky/web/HelloController.java
@@ -25,9 +25,10 @@ public class HelloController {
 
     @RequestMapping("/myweb")
     public String myweb(Model model) {
-        String ct = "## 建站\n" +
+        String ct = "建站\n" +
+                "===\n" +
                 "\n" +
-                "### 项目\n" +
+                "## 项目\n" +
                 "\n" +
                 "* 服务器\n" +
                 "* 域名\n" +
@@ -36,9 +37,9 @@ public class HelloController {
                 "* https\n" +
                 "\n" +
                 "\n" +
-                "### 服务器\n" +
+                "## 服务器\n" +
                 "\n" +
-                "* 虚拟主机\n" +
+                "### 虚拟主机\n" +
                 "\n" +
                 "    利用虚拟技术把一台完整的服务器分成若干个主机，拥有多个网站，共享这台服务器的硬件和带宽的资源，可以托管简单的静态和动态的网站，满足客户最基本的网络托管需求。虚拟主机通常是只有Apache/PHP/MySQL的网站部署环境，没有完整的系统管理和shell访问权限。\n" +
                 "    \n" +
@@ -46,7 +47,7 @@ public class HelloController {
                 "    \n" +
                 "    适用：个人博客，简单企业网站\n" +
                 "\n" +
-                "* VPS（Virtual Private Server）\n" +
+                "### VPS（Virtual Private Server）\n" +
                 "\n" +
                 "    将一台物理服务器分割成多个虚拟专属服务器，每个VPS主机分配独立操作系统，独立的IP地址和独立的应用程序，拥有系统的所有权限。\n" +
                 "    \n" +
@@ -54,7 +55,7 @@ public class HelloController {
                 "    \n" +
                 "    适用：个人博客，搭梯子，折腾\n" +
                 "\n" +
-                "* 云主机\n" +
+                "### 云主机\n" +
                 "\n" +
                 "    基于集群服务器，集群中每个主机都有云主机的一个镜像，可以提高虚拟主机的安全性和稳定性，具有更高的灵活性，用户可以自主定义服务器配置。\n" +
                 "    \n" +
@@ -62,19 +63,19 @@ public class HelloController {
                 "        \n" +
                 "    适用：大型网站首选\n" +
                 "\n" +
-                "### VPS\n" +
+                "### VPS购买\n" +
                 "\n" +
-                "* 选择供应商：国外：Vultr，Digital Ocean，banwagon等等\n" +
-                "* 购买VPS（注意选择机房）\n" +
-                "* 安装应用（ssh，java。。。等等）\n" +
+                "- 选择供应商：国外：Vultr，Digital Ocean，banwagon等等\n" +
+                "- 购买VPS（注意选择机房）\n" +
+                "- 安装应用（ssh，java。。。等等）\n" +
                 "\n" +
-                "### 域名\n" +
+                "## 域名\n" +
                 "\n" +
-                "* 尽量选择在国外网站（godaddy等等）上面购买域名，好处是不需要备案\n" +
+                "### 尽量选择在国外网站（godaddy等等）上面购买域名，好处是不需要备案\n" +
                 "\n" +
                 "![](http://ojoauf3ds.bkt.clouddn.com/a0ea7b49f53c2d7e.png?imageView2/3/w/200/h/200/q/100)\n" +
                 "\n" +
-                "* 设置DNS解析\n" +
+                "### 设置DNS解析\n" +
                 "\n" +
                 "![](http://ojoauf3ds.bkt.clouddn.com/063e7428f307f708.png?imageView2/3/w/200/h/400/q/100)\n" +
                 "\n" +
@@ -117,32 +118,22 @@ public class HelloController {
                 "http {\n" +
                 "    include       mime.types;\n" +
                 "    default_type  application/octet-stream;\n" +
-                "\n" +
-                "    #access_log  logs/access.log  main;\n" +
-                "\n" +
                 "    sendfile        on;\n" +
-                "\n" +
                 "    keepalive_timeout  65;\n" +
-                "\n" +
                 "    server {  \n" +
                 "            listen  80;  \n" +
                 "            server_name 67gogogo.com www.67gogogo.com;  \n" +
                 "            rewrite ^(.*)$  https://$host$1 permanent;  \n" +
                 "    }\n" +
-                "\n" +
-                "\n" +
                 "    server {\n" +
                 "    listen       443 ssl;\n" +
                 "    server_name www.67gogogo.com 67gogogo.com;\n" +
-                "\n" +
                 "    ssl_certificate      /root/tengxunssl/1_www.67gogogo.com_bundle.crt;\n" +
                 "    ssl_certificate_key  /root/tengxunssl/2_www.67gogogo.com.key;\n" +
                 "    ssl_session_timeout  5m;\n" +
-                "\n" +
-                "    ssl_protocols TLSv1 TLSv1.1 TLSv1.2; #按照这个协议配置\n" +
-                "    ssl_ciphers ECDHE-RSA-AES128-GCM-SHA256:HIGH:!aNULL:!MD5:!RC4:!DHE; #按照这个套件配置\n" +
+                "    ssl_protocols TLSv1 TLSv1.1 TLSv1.2;\n" +
+                "    ssl_ciphers ECDHE-RSA-AES128-GCM-SHA256:HIGH:!aNULL:!MD5:!RC4:!DHE;\n" +
                 "    ssl_prefer_server_ciphers  on;\n" +
-                "\n" +
                 "    location / {\n" +
                 "            proxy_redirect off;\n" +
                 "            proxy_set_header Host $host;\n" +
@@ -155,15 +146,15 @@ public class HelloController {
                 "}\n" +
                 "```\n" +
                 "\n" +
-                "### HTTPS\n" +
+                "## HTTPS\n" +
                 "\n" +
-                "* 对称加密和非对称加密\n" +
+                "### 对称加密和非对称加密\n" +
                 "\n" +
                 "\t对称加密：加密解密都用同一把密钥，加解密简单快速\n" +
                 "\n" +
                 "\t非对称加密：加密解密用不同的密钥，一般有两把密钥，公钥和私钥，加解密性能消耗较大\n" +
                 "\n" +
-                "* https连接建立过程\n" +
+                "### https连接建立过程\n" +
                 "\n" +
                 "![](http://ojoauf3ds.bkt.clouddn.com/5dba9bd04cf767cb.png?imageView2/3/w/200/h/300/q/100)\n" +
                 "\n" +
@@ -174,7 +165,8 @@ public class HelloController {
                 "5. 服务器用私钥解密得到对称密钥\n" +
                 "6. 用对称密钥传输数据\n" +
                 "\n" +
-                "* 中间人获取对称密钥过程\n" +
+                "\n" +
+                "### 中间人获取对称密钥过程\n" +
                 "\n" +
                 "![](http://ojoauf3ds.bkt.clouddn.com/29cad45b3c83c8e8.png?imageView2/3/w/200/h/300/q/100)\n" +
                 "\n" +
@@ -187,24 +179,24 @@ public class HelloController {
                 "之后客户端和服务器之间的所有密文全部都会被中间人解密器\n" +
                 "\n" +
                 "\n" +
-                "* CA认证\n" +
+                "### CA认证\n" +
                 "\n" +
                 "1. 服务器将自己的公钥送给认证机构（CA）进行认证\n" +
                 "2. 通过认证后CA会颁发证书，证书中包含服务器公钥，数字签名等等\n" +
                 "3. 服务器不再将公钥传送给客户端，而是将证书传送给客户端\n" +
                 "4. 客户端通过CA校验，判断证书是否伪造，并得到服务器公钥\n" +
                 "\n" +
-                "* 申请CA证书\n" +
+                "### 申请CA证书\n" +
                 "\n" +
-                "\t以腾讯SSL证书为例：[认证流程](https://www.qcloud.com/document/product/400/4142)\n" +
+                "以腾讯SSL证书为例：[认证流程](https://www.qcloud.com/document/product/400/4142)\n" +
                 "\t\n" +
                 "1. 提交申请\n" +
                 "2. 域名验证\n" +
                 "3. 下载证书\n" +
                 "\n" +
-                "\tNginx文件夹内获得SSL证书文件 .crt 和私钥文件 .key，将这两个文件放在Nginx服务器上，并在配置中启动ssl\n" +
+                "Nginx文件夹内获得SSL证书文件 .crt 和私钥文件 .key，将这两个文件放在Nginx服务器上，并在配置中启动ssl\n" +
                 "\n" +
-                "* DNS及CDN\n" +
+                "### DNS及CDN\n" +
                 "\n" +
                 "1. DNS解析及CDN解析过程\n" +
                 "\n" +
